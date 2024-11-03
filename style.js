e63bc7466f3b2b4ad0ab3d52f7c1713885140de3
klausqama <klaussqama@gmail.com>
2024-11-03 18:35:51 +0100

    add ex 2

--- style.js
+++ style.js
@@ -45,7 +45,33 @@ for(let i = 0; i < bici.length; i++){
 //Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
-
+const footballTeam = [
+    {
+        nome : 'juve', 
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome : 'milan',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome : 'inter', 
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome : 'roma', 
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome : 'napoli', 
+        punti: 0,
+        falli: 0,
+    },
+]
 
 //Snack 3 (Bonus)
 //Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
