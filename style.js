82c6b011d70bcf697db278d59e69ee34511c3fa8
klausqama <klaussqama@gmail.com>
2024-11-03 17:37:36 +0100

    add ex 1

--- style.js
+++ style.js
@@ -3,6 +3,41 @@
 //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore.
 
+const bici = [
+    {
+        nome : 'bici azzura', 
+        peso: 1.5
+    },
+    {
+        nome : 'bici gialla',
+        peso: 1.3
+    },
+    {
+        nome : 'bici verde', 
+        peso: 1.1
+    },
+    {
+        nome : 'bici grigia', 
+        peso: 1.8
+    },
+    {
+        nome : 'bici brutta', 
+        peso: 0.9,
+    },
+]
+let biciDiRiferimento = bici[0]
+
+
+for(let i = 0; i < bici.length; i++){
+    const biciCorrente = bici[i]
+    console.log(biciCorrente)
+
+    if(biciCorrente.peso < biciDiRiferimento.peso)
+    console.log(`La ${biciCorrente.nome}, e piu leggera della ${biciDiRiferimento.nome} `),
+    biciDiRiferimento = biciCorrente
+
+};
+
 //Snack2
 //Creare un array di oggetti di squadre di calcio.
 //Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
